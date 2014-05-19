6c8167ba46a67aa4acf3f27423fae567ad1553db
DrLance <megalance@gmail.com>
2014-05-19 15:35:26 +0400

    start

---
+++ Assets/Script/Main.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main : MonoBehaviour {
+
+	void OnGUI()
+	{
+		if(GUI.Button(new Rect(10,10,100,20),"Персонаж"))
+	   {
+			Application.OpenURL("ya.ru");
+		}
+	}
+}
